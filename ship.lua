fdea8a8f31757b16ecd52ec34ba0dcb47aaf4989
Luka Dornhecker <luka.dornhecker@gmail.com>
2014-02-03 15:24:54 +0100

    Some refactoring and tuning of ship movement

--- ship.lua
+++ ship.lua
@@ -4,7 +4,8 @@ class "Ship" {
 	rotation = -90;
 	vx = 0;
 	vy = 0;
-	speed = 15;
+	accel = 35;
+	friction = 0.09;
 	rotation_speed = 360;
 }
 
@@ -27,13 +28,13 @@ function Ship:update(dt, keys)
 
 	-- Apply forward thrust
 	if keys["up"] then
-		self.vx = self.vx + math.cos(math.rad(self.rotation)) * 40 * dt
-		self.vy = self.vy + math.sin(math.rad(self.rotation)) * 40 * dt
+		self.vx = self.vx + math.cos(math.rad(self.rotation)) * self.accel * dt
+		self.vy = self.vy + math.sin(math.rad(self.rotation)) * self.accel * dt
 	end
 
 	-- Apply friction
-	self.vx = self.vx * math.pow(0.05, dt)
-	self.vy = self.vy * math.pow(0.05, dt)
+	self.vx = self.vx * math.pow(self.friction, dt)
+	self.vy = self.vy * math.pow(self.friction, dt)
 
 	-- Calculate position by adding delta velcity
 	self.x = self.x + self.vx
